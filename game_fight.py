f9e90c4946f1066863b35e5d58ed9cd8c3e6be0a
trknvsky <trknvsky@gmail.com>
2020-01-23 22:45:40 +0200

    added location api in game

--- game_fight.py
+++ game_fight.py
@@ -1,8 +1,12 @@
 from game import *
+from api import *
 
 
 def main():
     fight = Fight()
+    geocode_api = GeocodeAPi()
+    location = input('Input location: ')
+    geocode_api.get_cords_by_location(location)
     fight.play()
 
 
